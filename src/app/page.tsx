535cb50b7db0c1432b0a935b63d4af0bec4214e4
sulaimancheming <sulaimanjehming@gmail.com>
2024-10-18 00:03:56 +0700

    welll done page reporter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,130 @@
+import { Fullscreen } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="NotoSan"></div>
-      <div>
-        <p>รายงานผลการตรวจสอบ</p>
+      {/* <div className="NotoSan">
+
+      </div> */}
+    <div className="-z-10 relative -top-10 ">
+        <div>
+        <p className="relative z-10 top-36 left-[625px] text-white text-3xl">รายงานผลการตรวจสอบ</p>
+        <p className="relative z-10 top-44 left-[520px] text-white ">ค่าตรวจวัดสูงสุดรายวัน ประจำวันจันทร์ ที่ 19 มิถุนายน 2566 เวลา 09:05 น.</p>
+      <Image src="/images/sulu2.png" alt=""  width={1000}  height={1000} className="w-full h-80 blur-[2px] " >
+      </Image>
+      <div className="flex gap-7 justify-center">
+
+      <div className=" relative z-10 -top-24  w-80 h-56 bg-white shadow-lg rounded-2xl ">
+        <div className="flex m-4 justify-between ">
+        <Image src="/images/sulu3.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        <p className="mt-2 text-blue-500">คุณภาพอากาศ</p>
+        <Image src="/images/sulu4.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        </div>
+        <div className="w-[80%] h-[2px] bg-slate-200 ml-7"></div>
+        <div className="flex justify-between m-4">
+          <div className="flex gap-2">
+            <p className="text-2xl font-bold">208</p>
+            <p className="mt-2">AQI</p>
+          </div>
+            <div className="w-20 h-6 text-center bg-red-300 rounded-xl">
+              <p className="text-sm">มีผลกระทบ</p>
+            </div>
+        </div>
+        <div className="m-4">
+          <p>วัดปลวกเกตุ</p>
+          <div className="flex font-light">
+            <p>อัพเดทล่าสุด:</p>
+            <p>19 ส.ค. 2564 19:00 น.</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <p>ค่าตรวจวัดสูงสุดรายวัน ประจำวันจันทร์ ที่ 19 มิถุนายน 2566 เวลา 09:05 น.</p>
+      <div className=" relative z-10 -top-24  w-80 h-56 bg-white shadow-lg rounded-2xl ">
+        <div className="flex m-4 justify-between ">
+        <Image src="/images/sulu3.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        <p className="mt-2 text-blue-500">คุณภาพอากาศ</p>
+        <Image src="/images/sulu4.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        </div>
+        <div className="w-[80%] h-[2px] bg-slate-200 ml-7"></div>
+        <div className="flex justify-between m-4">
+          <div className="flex gap-2">
+            <p className="text-2xl font-bold">208</p>
+            <p className="mt-2">AQI</p>
+          </div>
+            <div className="w-20 h-6 text-center bg-red-300 rounded-xl">
+              <p className="text-sm">มีผลกระทบ</p>
+            </div>
+        </div>
+        <div className="m-4">
+          <p>วัดปลวกเกตุ</p>
+          <div className="flex font-light">
+            <p>อัพเดทล่าสุด:</p>
+            <p>19 ส.ค. 2564 19:00 น.</p>
+          </div>
+        </div>
       </div>
+      <div className=" relative z-10 -top-24  w-80 h-56 bg-white shadow-lg rounded-2xl ">
+        <div className="flex m-4 justify-between ">
+        <Image src="/images/sulu3.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        <p className="mt-2 text-blue-500">คุณภาพอากาศ</p>
+        <Image src="/images/sulu4.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        </div>
+        <div className="w-[80%] h-[2px] bg-slate-200 ml-7"></div>
+        <div className="flex justify-between m-4">
+          <div className="flex gap-2">
+            <p className="text-2xl font-bold">208</p>
+            <p className="mt-2">AQI</p>
+          </div>
+            <div className="w-20 h-6 text-center bg-red-300 rounded-xl">
+              <p className="text-sm">มีผลกระทบ</p>
+            </div>
+        </div>
+        <div className="m-4">
+          <p>วัดปลวกเกตุ</p>
+          <div className="flex font-light">
+            <p>อัพเดทล่าสุด:</p>
+            <p>19 ส.ค. 2564 19:00 น.</p>
+          </div>
+        </div>
+      </div>
+      <div className=" relative z-10 -top-24  w-80 h-56 bg-white shadow-lg rounded-2xl ">
+        <div className="flex m-4 justify-between ">
+        <Image src="/images/sulu3.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        <p className="mt-2 text-blue-500">คุณภาพอากาศ</p>
+        <Image src="/images/sulu4.svg" alt=""  width={1000}  height={1000} className="w-10 " >
+        </Image>
+        </div>
+        <div className="w-[80%] h-[2px] bg-slate-200 ml-7"></div>
+        <div className="flex justify-between m-4">
+          <div className="flex gap-2">
+            <p className="text-2xl font-bold">208</p>
+            <p className="mt-2">AQI</p>
+          </div>
+            <div className="w-20 h-6 text-center bg-red-300 rounded-xl">
+              <p className="text-sm">มีผลกระทบ</p>
+            </div>
+        </div>
+        <div className="m-4">
+          <p>วัดปลวกเกตุ</p>
+          <div className="flex font-light">
+            <p>อัพเดทล่าสุด:</p>
+            <p>19 ส.ค. 2564 19:00 น.</p>
+          </div>
+        </div>
+      </div>
+
+
+      </div>
+      </div>
+    </div>
+   
     </>
   );
 }
--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,135 +1,196 @@
-"use client";  // เพิ่มบรรทัดนี้เพื่อบอกว่าไฟล์นี้เป็น Client Component
+"use client";
+import Image from "next/image";
+import { Input, Select, Upload } from "antd";
+import React, { useState } from "react";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import { Flex, message } from "antd";
+import type { GetProp, UploadProps } from "antd";
 
-import React, { useState } from 'react';
-import { AutoComplete } from 'antd';
-import type { AutoCompleteProps } from 'antd';
-import type { CascaderProps } from 'antd';
-import { Cascader } from 'antd';
-import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
-import { Select, Space } from 'antd';
+type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
 
+const getBase64 = (img: FileType, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
 
-export default function Report() {
-
-  type FieldType = {
-    username?: string;
-    number?: string;
-    remember?: string;
-  };
-
-  const { Option } = Select;
-  const selectBefore = (
-    <Select defaultValue="http://">
-      <Option value="http://">+66</Option>
-      <Option value="https://">+55</Option>
-    </Select>
-  );
-
-  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
-
-  interface Option {
-    value: string;
-    label: string;
-    children?: Option[];
+const beforeUpload = (file: FileType) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
   }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
 
-  const options: Option[] = [
-    {
-      value: 'zhejiang',
-      label: 'Zhejiang',
-      // children: [
-      //   {
-      //     value: 'hangzhou',
-      //     label: 'Hangzhou',
-      //     children: [
-      //       {
-      //         value: 'xihu',
-      //         label: 'West Lake',
-      //       },
-      //     ],
-      //   },
-      // ],
-    },
-    {
-      value: 'jiangsu',
-      label: 'Jiangsu',
-      // children: [
-      //   {
-      //     value: 'nanjing',
-      //     label: 'Nanjing',
-      //     children: [
-      //       {
-      //         value: 'zhonghuamen',
-      //         label: 'Zhong Hua Men',
-      //       },
-      //     ],
-      //   },
-      // ],
-    },
-  ];
+const { Option } = Select;
+const selectBefore = (
+  <Select defaultValue="66">
+    <Option value="66">66</Option>
+    <Option value="67">65</Option>
+  </Select>
+);
+export default function Login() {
 
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const handleChange: UploadProps["onChange"] = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj as FileType, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+  const uploadButton = (
+    <button className="" style={{ border: 0, background: "none" }} type="button">
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </button>
+  );
 
   return (
     <>
-      <div className="pt-10 ">
-        <p className="text-center truncate h-10 text-2xl font-extrabold">การแนะนำติชม</p>
-        <p className="text-center truncate p-7">กรอกข้อมูล</p>
+      <div className="">
+        {/* <Image
+          src="/images/Contentbackground.svg"
+          alt={""}
+          width={758}
+          height={758}
+          className="absolute left-[26%] w-full max-w-[758px] h-auto "
+        ></Image> */}
+        <div className="flex flex-col items-center justify-center relative z-[1] pt-5 pb-5 ">
+          <div className="text-center mb-6">
+            <h2 className="text-[30px] font-bold mb-2"> การแนะนำติชม</h2>
+            <p className="text-gray-600 text-[16px]">กรอกข้อมูล</p>
+          </div>
+          <div className="p-8 rounded-lg w-full max-w-md">
+            <form>
+              <div className="mb-4">
+                <label htmlFor="fullName" className="block text-gray-700 mb-2">
+                  ชื่อและนามสกุล
+                </label>
+                <Input
+                  className="p-3 w-full"
+                  placeholder="กรอกชื่อและนามสกุล"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="phone" className="block text-gray-700 mb-2">
+                  เบอร์โทรศัพท์
+                </label>
+                <div className="flex">
+                  <Input
+                    addonBefore={selectBefore}
+                    classNames={{ input: "w-full p-3 " }}
+                  />
+                </div>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="phone" className="block text-gray-700 mb-2">
+                  ประเทภการร้องเรียน
+                </label>
+                <Select className="w-full h-11" placeholder="เลือกประเภท">
+                  <Select.Option value="sample1">แนะนำ,ติชม</Select.Option>
+                  <Select.Option value="sample2">ร้องเรียน</Select.Option>
+                  <Select.Option value="sample3">
+                    เจ้าหน้าที่ติดต่อกลับ
+                  </Select.Option>
+                </Select>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="phone" className="block text-gray-700 mb-2">
+                  ข้อมูลเพิ่มเติม
+                </label>
+                <textarea
+                  placeholder="กรอกข้อมูล"
+                  className="p-1 border-2 rounded-lg textarea textarea-bordered textarea-xl w-full h-[80px]  "
+                ></textarea>
+              </div>
+              <div className="flex mb-4">
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader w-full"
+                  showUploadList={false}
+                  action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                >
+                  {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt="avatar"
+                      style={{ width: "100%" }}
+                    />
+                  ) : (
+                    uploadButton 
+                  )}
+                </Upload>
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader w-full"
+                  showUploadList={false}
+                  action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                >
+                  {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt="avatar"
+                      style={{ width: "100%" }}
+                    />
+                  ) : (
+                    uploadButton 
+                  )}
+                </Upload>
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  className="avatar-uploader w-full"
+                  showUploadList={false}
+                  action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+                  beforeUpload={beforeUpload}
+                  onChange={handleChange}
+                >
+                  {imageUrl ? (
+                    <img
+                      src={imageUrl}
+                      alt="avatar"
+                      style={{ width: "100%" }}
+                    />
+                  ) : (
+                    uploadButton 
+                  )}
+                </Upload>
+              </div>
+                  {/* <p>จำกัดแค่สามรูป</p> */}
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition"
+              >
+                ส่งข้อมูล
+              </button>
+            </form>
+          </div>
+          <div className="text-center ">
+            <p className="underline decoration-1 cursor-pointer  text-center">
+              ประวัติการใช้งาน
+            </p>
+          </div>
+        </div>
       </div>
-
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <p className="relative top-0 left-[574px]">ชื่อและนามสกุล</p>
-        <div className='relative top-0 left-[574px]'>
-          <Form.Item<FieldType >
-            name="username"
-            rules={[{ required: true, message: 'กรุณากรอกชื่อและนามสกุล!' }]}
-            style={{ width: '100%', maxWidth: '600px' }}
-          >
-            <Input placeholder="กรอกชื่อและนามสกุล" className="w-full" />
-          </Form.Item>
-        </div>
-
-        <p className="relative top-0 left-[574px]">เบอร์โทรศัพท์</p>
-        <div className='relative top-0 left-[574px] pb-7 w-[400px]'>
-        
-            <Input addonBefore={selectBefore} classNames={{input : "w-[full] "} }
-              
-              defaultValue="" placeholder='000-000-0000' />
-        </div>
-
-        <p className="relative top-0 left-[574px] text-sm">ประเทภการร้องเรียน</p>
-        <div className="relative top-0 left-[574px]">
-          <Cascader options={options}  style={{ width: 400 }} placeholder="เลือกประเภท" />
-        </div>
-
-        <p className="relative top-0 left-[574px] text-sm pt-7">ข้อมูลเพิ่มเติม</p>
-        <textarea placeholder=""
-          className="textarea textarea-bordered textarea-xl w-[400px] h-[80px]  relative top-0 left-[574px]"></textarea>
-       
-        <div className='relative top-0 left-[375px] pt-3'>
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit" style={{ width: '100%', maxWidth: '600px' }}>
-              ส่งข้อมูล
-            </Button>
-          <p className='underline decoration-1 cursor-pointer m-5 text-center'>ประวัติการใช้งาน</p>
-          </Form.Item>
-        </div>
-      </Form>
     </>
   );
 }
