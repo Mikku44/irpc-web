669bd1ee2e30a1b4c0d0cde40bd453f6b28b4031
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2024-11-12 11:11:03 +0700

    fixed Deflect

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
           <div className="lg:flex  md:grid grid justify-between items-center px-0">
             <div className="flex flex-col items-start mb-6 md:mb-0 text-left lg:ml-20 ml-10">
               <img src="/images/irpc-logo.svg" alt="IRPC Logo" className="h-8 mb-4" />
-              <p className="text-gray-600 text-sm max-w-xs">Design amazing digital experiences that create more happy in the world.</p>
+              <p className="text-gray-600 text-[16px] max-w-[21rem]">ไออาร์พีซี เป็นผู้ประกอบการอุตสาหกรรมปิโตรเคมี &quot;ครบวงจรแห่งแรก&quot; ของภูมิภาคเอเชียตะวันออกเฉียงใต้</p>
               <div className="lg:flex md:grid grid flex-wrap justify-start lg:space-x-7 gap-4 mt-4">
                 <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Overview</a>
                 <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Features</a>
-                <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Pricing</a>
+                {/* <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Pricing</a>
                 <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Careers</a>
-                <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Help</a>
+                <a href="#" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Help</a> */}
                 <Link href="/privacy-policy/th" className="font-bold text-gray-700 hover:text-blue-500 text-sm">Privacy</Link>
               </div>
             </div>
@@ -23,10 +23,10 @@ export default function Footer() {
             <div className="lg:flex md:grid grid lg:p-0 md:px-20 px-10 flex-col items-end text-left mr-20">
               <div className="flex flex-col space-y-2">
                 <p className="text-blue-500 mb-2 ">Get the app</p>
-                <a href="#">
+                <a target="_blank" href="https://apps.apple.com/th/app/irpc-air/id1494650415">
                   <img src="/images/appstore.svg" alt="App Store" className="h-10" />
                 </a>
-                <a href="#">
+                <a target="_blank" href="https://play.google.com/store/apps/details?id=th.irpc.irpc_pollution&pcampaignid=web_share">
                   <img src="/images/googleplay.svg" alt="Google Play" className="h-10" />
                 </a>
               </div>
@@ -37,13 +37,13 @@ export default function Footer() {
             <div className="w-full flex flex-col md:flex-row justify-between items-center text-gray-600">
               <p className="text-sm ">&copy; 2024 IRPC, Inc. All rights reserved.</p>
               <div className="flex space-x-6 lg:pt-0 md:pt-0 pt-3">
-                <a href="#" className="hover:text-blue-500">
+                <a href="https://x.com/IRPCofficial" className="hover:text-blue-500">
                   <img src="/images/x.svg" alt="X" className="h-5" />
                 </a>
-                <a href="#" className="hover:text-blue-500">
+                <a href="https://www.facebook.com/IRPCofficial/" className="hover:text-blue-500">
                   <img src="/images/linkedin.svg" alt="LinkedIn" className="h-5" />
                 </a>
-                <a href="#" className="hover:text-blue-500">
+                <a href="https://www.instagram.com/irpcofficial/" className="hover:text-blue-500">
                   <img src="/images/facebook.svg" alt="Facebook" className="h-5" />
                 </a>
               </div>
